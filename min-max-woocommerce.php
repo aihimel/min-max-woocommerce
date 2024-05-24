34454d31fb63e9cdd057086baaff72b7a65d242f
Aftabul Islam <toaihimel@gmail.com>
2024-05-24 17:58:42 +0600

    Composer initiated

---
+++ min-max-woocommerce.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Min Max WooCommerce
+ * Plugin Author: Aftabul Islam
+ * Text Domain: min-max-woocommerce
+ * Version: 1.0.0
+ * Description: Minimum & maximum restriction for WooCommerce, WordPress plugin
+ * Requires Plugins: woocommerce
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+require_once "vendor/autoload.php";
