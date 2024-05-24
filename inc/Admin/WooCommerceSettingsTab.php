b4f268c9a4add0e2da2bebe9bb2d2663b02896b4
Aftabul Islam <toaihimel@gmail.com>
2024-05-24 22:53:05 +0600

    feat: Settings added

--- inc/Admin/WooCommerceSettingsTab.php
+++ inc/Admin/WooCommerceSettingsTab.php
@@ -17,9 +17,69 @@ class WooCommerceSettingsTab {
 	const INSTANCE_KEY = 'woocommerce-settings-tab';
 	const MIN_MAX_SETTINGS_PAGE_KEY = 'min-max-settings';
 
+	/**
+	 * Settings ids
+	 *
+	 * @since MIN_MAX_SINCE
+	 */
+	const TITLE_ID = 'min-max-woocommerce-settings-title';
+	const MINIMUM_AMOUNT_ID = 'min-max-woocommerce-settings-minimum-amount';
+	const MAXIMUM_AMOUNT_ID = 'min-max-woocommerce-settings-maximum-amount';
+
 	public function __construct() {
 		add_filter( 'woocommerce_get_sections_products', [ $this, 'add_min_max_menu' ], 11 );
-		add_filter( 'woocommerce_settings_products', [ $this, 'render_min_max_settings_page' ], 21 );
+		add_filter( 'woocommerce_get_settings_products', [ $this, 'add_min_max_settings' ], 10, 2 );
+	}
+
+	/**
+	 * Adds min max settings inside `min-max-settings` tab
+	 *
+	 * @since MIN_MAX_SINCE
+	 *
+	 * @param $settings
+	 * @param $current_section
+	 *
+	 * @return mixed
+	 */
+	public function add_min_max_settings( $settings, $current_section ) {
+		if ( ! self::MIN_MAX_SETTINGS_PAGE_KEY === $current_section ) {
+			return $settings;
+		}
+		$settings[] = [
+			'name' => __('Cart Amount Settings', 'min-max-woocommerce'),
+			'type' => 'title',
+			'desc' => __('Set minimum and maximum cart amount for your customers', 'min-max-woocommerce'),
+			'id'   => self::TITLE_ID
+		];
+
+		$settings[] = [
+			'name'     => __('Minimum Amount', 'min-max-woocommerce'),
+			'desc_tip' => __('Enter the cart minimum amount for your customers.', 'min-max-woocommerce'),
+			'id'       => self::MINIMUM_AMOUNT_ID,
+			'type'     => 'text',
+			'css'      => 'min-width:300px;',
+			'desc'     => __('Cart minimum amount', 'min-max-woocommerce'),
+			'default'  => '',
+			'placeholder' => 'eg: 30.00'
+		];
+
+		$settings[] = [
+			'name'     => __('Maximum Amount', 'min-max-woocommerce'),
+			'desc_tip' => __('Enter the cart maximum amount for your customers.', 'min-max-woocommerce'),
+			'id'       => self::MAXIMUM_AMOUNT_ID,
+			'type'     => 'text',
+			'css'      => 'min-width:300px;',
+			'desc'     => __('Cart maximum amount', 'min-max-woocommerce'),
+			'default'  => '',
+			'placeholder' => 'eg: 300.00'
+		];
+
+		$settings[] = array(
+			'type' => 'sectionend',
+			'id'   => 'my_custom_product_settings'
+		);
+
+		return $settings;
 	}
 
 	/**
@@ -35,21 +95,4 @@ class WooCommerceSettingsTab {
 		$sections[ self::MIN_MAX_SETTINGS_PAGE_KEY ] = __( 'Min Max', 'min-max-woocommerce' );
 		return $sections;
 	}
-
-	/**
-	 * Renders the settings page
-	 *
-	 * @since MIN_MAX_SINCE
-	 *
-	 * @return void
-	 */
-	public function render_min_max_settings_page() {
-		if ( isset( $_GET[ 'section' ] ) ) {
-			$section = sanitize_text_field( $_GET[ 'section' ] );
-			if ( self::MIN_MAX_SETTINGS_PAGE_KEY === $section ) {
-				Template::load( 'admin/settings' );
-			}
-		}
-	}
-
 }
