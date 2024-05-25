c715ec398f0bd8d801f14fbb99d1e80a124f227c
Aftabul Islam <toaihimel@gmail.com>
2024-05-25 09:38:26 +0600

    Amount resriction added

---
+++ inc/Restrictions/AmountRestriction.php
@@ -0,0 +1,15 @@
+<?php
+namespace Aihimel\MinMax;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Adds amount restriction on frontend
+ *
+ * @package aihimel/min-max
+ *
+ * @since MIN_MAX_SINCE
+ */
+class AmountRestriction {
+
+}
